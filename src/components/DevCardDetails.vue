0d0d7094e4d87a8f10313b7addab4530b52b680a
Kamil Bania <kamilbania2@gmail.com>
2021-09-21 01:26:31 +0200

    Dev Card Details layout change with development progress indicators

--- src/components/DevCardDetails.vue
+++ src/components/DevCardDetails.vue
@@ -7,73 +7,152 @@
         <v-icon color="primary" small>mdi-timer-sand</v-icon>
       </v-card-text>
       <v-card-text>
-        <v-row align="center" justify="center">
+        <v-row>
           <v-col cols="8">
-            <v-divider></v-divider>
-
-            <v-list>
-              <v-list-item
-                v-for="(gameCardComponent, index) in card.gameCardComponents"
-                :key="`$game-card-component-${index}`"
-              >
-                <v-list-item-avatar>
-                  <v-icon :color="gameCardComponent.gec.color">{{
-                    gameCardComponent.gec.icon
-                  }}</v-icon>
-                </v-list-item-avatar>
-                <v-list-item-content>
-                  <v-list-item-title class="text-left">
-                    <span v-if="gameCardComponent.title !== ''">
-                      {{ gameCardComponent.title }}
-                    </span>
-                    <span v-else>
-                      {{ gameCardComponent.gec.name }}
-                    </span>
-                  </v-list-item-title>
-                </v-list-item-content>
-                <v-list-item-action>
-                  <change-component-state-menu :componentState="gameCardComponent.cardState"></change-component-state-menu>
-                </v-list-item-action>
-              </v-list-item>
-              <v-divider class="my-2"></v-divider>
-              <v-list-group
-                v-for="(state, index) in card.gameEntityFSM.states"
-                :key="`game-entity-state-${index}`"
-                prepend-icon="mdi-state-machine"
-                class="ml-2"
-              >
-                <template v-slot:activator>
-                  <v-list-item-content class="text-left">
-                    <v-list-item-title>{{ state.name }}</v-list-item-title>
-                  </v-list-item-content>
-                </template>
-
-                <v-list-item
-                  v-for="(gameCardComponent, index) in state.gameCardComponents"
-                  :key="`$state-game-card-component-${index}`"
-                  class="pl-12"
-                >
-                  <v-list-item-avatar>
-                    <v-icon :color="gameCardComponent.gec.color">{{
-                      gameCardComponent.gec.icon
-                    }}</v-icon>
-                  </v-list-item-avatar>
-                  <v-list-item-content>
-                    <v-list-item-title class="text-left">
-                      <span v-if="gameCardComponent.title !== ''">
-                        {{ gameCardComponent.title }}
-                      </span>
-                      <span v-else>
-                        {{ gameCardComponent.gec.name }}
-                      </span>
-                    </v-list-item-title>
-                  </v-list-item-content>
-                  <v-list-item-action>
-                    <change-component-state-menu :componentState="gameCardComponent.cardState"></change-component-state-menu>
-                  </v-list-item-action>
-                </v-list-item>
-              </v-list-group>
-            </v-list>
+            <v-row>
+              <v-col cols="6">Component</v-col>
+              <v-col cols="6">
+                <v-row>
+                  <v-col cols="12">Progress</v-col>
+                  <v-col cols="12">
+                    <v-row>
+                      <v-col
+                        ><v-icon color="primary"
+                          >mdi-pencil-ruler</v-icon
+                        ></v-col
+                      >
+                      <v-col><v-icon color="primary">mdi-tools</v-icon></v-col>
+                      <v-col
+                        ><v-icon color="primary"
+                          >mdi-diamond-stone</v-icon
+                        ></v-col
+                      >
+                    </v-row>
+                  </v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+            <v-row
+              v-for="(gameCardComponent, index) in card.gameCardComponents"
+              :key="`$game-card-component-${index}`"
+            >
+              <v-col cols="2"
+                ><v-icon :color="gameCardComponent.gec.color">{{
+                  gameCardComponent.gec.icon
+                }}</v-icon>
+              </v-col>
+              <v-col cols="4" class="text-left"
+                ><span v-if="gameCardComponent.title !== ''">
+                  {{ gameCardComponent.title }}
+                </span>
+                <span v-else>
+                  {{ gameCardComponent.gec.name }}
+                </span>
+              </v-col>
+              <v-col cols="6" class="d-flex align-center">
+                <v-row>
+                  <v-col>
+                    <v-progress-linear
+                      :value="
+                        gameCardComponent.cardState === CARD_STATES.PROTOTYPE ||
+                        gameCardComponent.cardState === CARD_STATES.TO_DO
+                          ? 100
+                          : 0
+                      "
+                    ></v-progress-linear>
+                  </v-col>
+                  <v-col
+                    ><v-progress-linear
+                      :value="
+                        gameCardComponent.cardState === CARD_STATES.DONE ||
+                        gameCardComponent.cardState === CARD_STATES.TO_POLISH
+                          ? 100
+                          : 0
+                      "
+                    ></v-progress-linear
+                  ></v-col>
+                  <v-col
+                    ><v-progress-linear
+                      :value="
+                        gameCardComponent.cardState === CARD_STATES.POLISHED
+                          ? 100
+                          : 0
+                      "
+                    ></v-progress-linear
+                  ></v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+            <div
+              v-for="(state, index) in card.gameEntityFSM.states"
+              :key="`game-entity-state-${index}`"
+            >
+              <v-row>
+                <v-col cols="2" class="d-flex justify-center">
+                  <v-icon color="primary"> mdi-state-machine </v-icon>
+                </v-col>
+                <v-col cols="10" class="text-left">
+                  <v-expansion-panels tile flat>
+                    <v-expansion-panel>
+                      <v-expansion-panel-header class="px-1">
+                        {{ state.name }}
+                      </v-expansion-panel-header>
+                      <v-expansion-panel-content>
+                        <v-row
+                          v-for="(
+                            gameCardComponent, index
+                          ) in state.gameCardComponents"
+                          :key="`$state-game-card-component-${index}`"
+                        >
+                          <v-col cols="2"
+                            ><v-icon :color="gameCardComponent.gec.color">{{
+                              gameCardComponent.gec.icon
+                            }}</v-icon>
+                          </v-col>
+                          <v-col cols="4" class="text-left"
+                            ><span v-if="gameCardComponent.title !== ''">
+                              {{ gameCardComponent.title }}
+                            </span>
+                            <span v-else>
+                              {{ gameCardComponent.gec.name }}
+                            </span>
+                          </v-col>
+                         <v-col>
+                    <v-progress-linear
+                      :value="
+                        gameCardComponent.cardState === CARD_STATES.PROTOTYPE ||
+                        gameCardComponent.cardState === CARD_STATES.TO_DO
+                          ? 100
+                          : 0
+                      "
+                    ></v-progress-linear>
+                  </v-col>
+                  <v-col
+                    ><v-progress-linear
+                      :value="
+                        gameCardComponent.cardState === CARD_STATES.DONE ||
+                        gameCardComponent.cardState === CARD_STATES.TO_POLISH
+                          ? 100
+                          : 0
+                      "
+                    ></v-progress-linear
+                  ></v-col>
+                  <v-col
+                    ><v-progress-linear
+                      :value="
+                        gameCardComponent.cardState === CARD_STATES.POLISHED
+                          ? 100
+                          : 0
+                      "
+                    ></v-progress-linear
+                  ></v-col>
+                        </v-row>
+                      </v-expansion-panel-content>
+                    </v-expansion-panel>
+                  </v-expansion-panels>
+                </v-col>
+              </v-row>
+            </div>
           </v-col>
         </v-row>
       </v-card-text>
@@ -81,7 +160,7 @@
   </div>
 </template>
 <script>
-import ChangeComponentStateMenu from "@/components/ChangeComponentStateMenu";
+// import ChangeComponentStateMenu from "@/components/ChangeComponentStateMenu";
 
 export default {
   props: {
@@ -90,7 +169,13 @@ export default {
   },
 
   components: {
-    "change-component-state-menu": ChangeComponentStateMenu,
+    // "change-component-state-menu": ChangeComponentStateMenu,
+  },
+
+  computed: {
+    CARD_STATES() {
+      return this.$store.getters["cardComponents/CARD_STATES"];
+    },
   },
 };
 </script>
